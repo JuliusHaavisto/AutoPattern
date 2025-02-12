87ae222fd4c7680c88e2c9571ba12af5b6eca59a
Julius Haavisto <123388085+JuliusHaavisto@users.noreply.github.com>
2025-02-12 12:10:10 +0200

    Added SQL capabilities and ensured DB privacy

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 from objectdetect import detected_objects, display_image
 from objectcount import objectcount
 def main():
-    image_path = "data/triangles.jpg"
+    image_path = "data/triangle.jpg"
     detected_image = detected_objects(image_path)
     count = objectcount(image_path)
     print(f"Total objects detected: {count}")
